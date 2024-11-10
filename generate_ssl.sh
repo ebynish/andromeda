47529b0a9cac4d1206a0d0790b188ed580fa51e8
ebynish <ebenish4real@gmail.com>
2024-11-10 15:38:45 +0100

    'docker-set'

---
+++ generate_ssl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set domain names for which you want to generate SSL certificates
+DOMAINS=("keycloak.dkrrs.net" "andromeda.dkrrs.net")
+WEBROOT_PATH="/var/www/certbot"
+NGINX_CONF_DIR="/etc/nginx"
+
+# Run Certbot to generate SSL certificates
+docker-compose run --rm certbot certonly --webroot \
+  --webroot-path="$WEBROOT_PATH" \
+  --email your-email@example.com \
+  --agree-tos \
+  --no-eff-email \
+  "${DOMAINS[@]/#/--domains=}"
+
+# Reload Nginx to apply the newly generated certificates
+docker-compose exec nginx nginx -s reload
+
+# Print success message
+echo "SSL certificates generated and Nginx reloaded successfully!"
